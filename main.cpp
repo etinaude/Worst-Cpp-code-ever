0aee1194944921be63a0c38ae0c19e8f6ce724b2
Etienne Naude <etinaude@gmail.com>
2019-07-14 13:33:06 +1200

    Just a start to see how well #define works
    Memes will be plentyful

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define SOME printf
+#define BODY (
+#define ONCE "hello world"
+#define TOLD )
+#define ME ;
+//clear; gcc -o output main.c; printf '\n'; ./output; printf '\n \n'
+int main(){
+    SOME BODY ONCE TOLD ME
+}
