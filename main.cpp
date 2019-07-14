f37d4f2b208d6725e78e19b725d433410bc8bce2
Etienne Naude <etinaude@gmail.com>
2019-07-14 14:32:41 +1200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
 #include <stdio.h>
-#define SOME printf
-#define BODY (
-#define ONCE "hello world"
+#include <string>
+//#include <iostream> 
+#include <sstream>
+using namespace std;
+
+#define SOME int
+#define BODY main
+#define ONCE ( 
 #define TOLD )
-#define ME ;
+#define ME {
+
+#define THE char
+#define WORLD str
+#define WAS [
+#define GONNA ]
+#define ROLL =
+#define I "hello world!"
+
+#define AINT ;
+#define SHARPEST printf 
+#define TOOL (
+#define IN "%s"
+#define SHED ,
+#define WELL WORLD
+#define YEARS )
+#define START AINT
+#define COMING }
+
+//#define AINT
 //clear; gcc -o output main.c; printf '\n'; ./output; printf '\n \n'
-int main(){
-    SOME BODY ONCE TOLD ME
-}
+SOME BODY ONCE TOLD ME
+    THE WORLD WAS GONNA ROLL I AINT
+    SHARPEST TOOL IN SHED WELL YEARS START
+COMING
