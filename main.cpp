da6d3f7a369e1399aa2f1e74ff042146d56381f3
Etienne Naude <etinaude@gmail.com>
2019-07-14 15:53:22 +1200

    added more

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
+//clear; gcc -o output main.c; printf '\n'; ./output; printf '\n \n'
 #include <stdio.h>
-#include <string>
-//#include <iostream> 
+#include <string> 
 #include <sstream>
 using namespace std;
 
@@ -9,27 +9,93 @@ using namespace std;
 #define ONCE ( 
 #define TOLD )
 #define ME {
-
 #define THE char
 #define WORLD str
 #define WAS [
 #define GONNA ]
 #define ROLL =
 #define I "hello world!"
-
 #define AINT ;
 #define SHARPEST printf 
 #define TOOL (
-#define IN "%s"
+#define IN "%s\n\n"
 #define SHED ,
-#define WELL WORLD
-#define YEARS )
-#define START AINT
-#define COMING }
 
-//#define AINT
-//clear; gcc -o output main.c; printf '\n'; ./output; printf '\n \n'
+#define LOOKING WORLD
+#define KINDA )
+#define DUMB AINT
+
+#define WITH int
+#define HER arr[]=
+#define FINGER {1,3,2,5,4,6,7,8,9,0};
+#define AND DRUMS(
+#define THUMB arr);
+#define SHAPE }
+
+
+#define HEAR   int
+#define DRUMS bubble
+#define ECHOING (
+#define TONIGHT int
+#define BUT inp[])
+#define SHE {
+#define HEARS int
+#define ONLY temp
+#define WHISPERS =
+#define OF 0
+#define QUIET ;
+#define CONVERSATION for(int
+#define SHES i=0;
+#define TWELVE i 
+#define THIRTY <10;
+#define FLIGHT i++){
+#define MOONLIT for(int
+#define WINGS j
+#define REFLECTS =0;
+#define STARS j<10;
+#define THAT j++){
+#define GUIDE  if(inp[j]
+#define TOWARDS >inp[j+1]){
+#define SALVATION temp
+#define STOPPED =inp[j];  
+#define AN inp[j]
+#define OLD =inp[j+1];
+#define MAN inp[j+1]
+#define ALONG =temp;}
+#define WAY }}
+#define HOPING for(int
+#define FIND i=0;
+#define FORGOTTEN i<10;
+#define WORDS i++){
+#define OR printf(
+#define ANCIENT "%d\n"
+#define MELODIES ,inp[i]);}
+#define HE return
+#define TURNED 0;}
+
+HEAR DRUMS ECHOING TONIGHT  BUT SHE //bubble sort finish later
+    HEARS ONLY WHISPERS OF QUIET 
+    CONVERSATION SHES TWELVE THIRTY FLIGHT
+        MOONLIT WINGS REFLECTS STARS THAT
+            GUIDE TOWARDS 
+                SALVATION STOPPED
+                AN OLD
+                MAN ALONG WAY
+    HOPING FIND FORGOTTEN WORDS
+    OR ANCIENT MELODIES
+    HE TURNED
+    
+
+
+
+
+
 SOME BODY ONCE TOLD ME
     THE WORLD WAS GONNA ROLL I AINT
-    SHARPEST TOOL IN SHED WELL YEARS START
-COMING
+    SHARPEST TOOL IN SHED LOOKING KINDA DUMB
+    WITH HER FINGER
+    AND THUMB
+    SHAPE
+//insert it here
+
+
